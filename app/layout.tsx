4a502267cb046b6b25ca9954acefd89d17c07076
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-03-16 21:07:13 +0900

    Todo Header 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TodoHeader from "./components/todo/layout/TodoHeader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
+        <TodoHeader />
         {children}
       </body>
     </html>
