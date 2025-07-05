c8d865367d6a70ffe08a4afb453e8c2a2bf34301
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-07-05 18:50:12 +0900

    fix: 사용하지 않는 import 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import TodoHeader from "./components/todo/layout/TodoHeader";
 import Providers from "./providers";
 
 const geistSans = Geist({
