17337328104c7c89a1c1a30e684d949d51d7fc1f
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-07-05 19:09:15 +0900

    fix

--- app/query/useTodoQuery.ts
+++ app/query/useTodoQuery.ts
@@ -1,22 +1,22 @@
-import { getTodoItem, getTodos } from '@/api/todo-api';
-import { FilterType } from '@/store/useTodoFilterStore';
-import { Todo } from '@/types/todo.type';
-import { createClient } from '@/utils/supabase/client';
-import { useQuery } from '@tanstack/react-query';
+import { getTodoItem, getTodos } from "@/api/todo-api";
+import { FilterType } from "@/store/useTodoFilterStore";
+import { Todo } from "@/types/todo.type";
+import { useQuery } from "@tanstack/react-query";
+import { createClient } from "@/utils/supabase/client";
 
 export const useTodosQuery = (filter: FilterType) => {
   const SupabaseClient = createClient();
   // 외부에서 데이터를 가져오는 hook이다.
   return useQuery({
-    queryKey: ['todos', filter],
+    queryKey: ["todos", filter],
     queryFn: () => getTodos(SupabaseClient, filter),
   });
 };
 
-export const useTodoItemQuery = (id: Todo['id']) => {
+export const useTodoItemQuery = (id: Todo["id"]) => {
   const SupabaseClient = createClient();
   return useQuery({
-    queryKey: ['todos', id],
+    queryKey: ["todos", id],
     queryFn: () => getTodoItem(SupabaseClient, id),
   });
 };
